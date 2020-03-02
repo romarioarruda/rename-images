3a70c1750ef11316fccee87fe68849973462ac1f
Romário Arruda <romario.arruda@fabricadeideias.com>
2020-03-02 11:08:31 -0300

    Dinamizando numero inicial da página

--- rename.php
+++ rename.php
@@ -2,14 +2,19 @@
 
 $currentFormat = $argv[1];
 $pageName   = 'A';
+$pg = 1;
 
-if (isset($argv[2])) {
+if (!empty($argv[2])) {
 	$pageName = $argv[2];
+}
+
+if (!empty($argv[3])) {
+	$pg = $argv[3];
 }
 
 $files = glob("{*.$currentFormat}", GLOB_BRACE);
 
-$pg = 1;
+
 foreach($files as $img){
 	$renamed = str_replace($img, $pageName.$pg.".".$currentFormat, $img);
 
